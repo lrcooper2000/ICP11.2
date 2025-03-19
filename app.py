41a6c761e169c91fefe6c1a11dd0a496ad560952
lrcooper2000 <qlp726@mocs.utc.edu>
2025-03-19 13:19:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import tensorflow as tf
 # model_selected = keras.models.load_model('model_selected.h5',
 custom_objects={'mse': tf.keras.losses.mean_squared_error}
 # model_all = keras.models.load_model('model_all.h5', custom_objects={'mse':
-tf.keras.losses.mean_squared_error)
+tf.keras.losses.mean_squared_error
 model_selected = keras.models.load_model('model_selected.h5')
 model_all = keras.models.load_model('model_all.h5')
 preprocessor_selected = joblib.load('preprocessor_selected.pkl')
